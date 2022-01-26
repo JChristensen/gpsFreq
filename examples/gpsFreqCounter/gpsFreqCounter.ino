0951075d55c5bd84b0fbbd42ccb406cd066e4c20
JChristensen <jack.christensen@outlook.com>
2022-01-26 17:32:52 -0500

    Update examples, add src directory. Add files: LICENSE, CONTRIBUTING, library.properties, .gitignore

--- examples/gpsFreqCounter/gpsFreqCounter.ino
+++ examples/gpsFreqCounter/gpsFreqCounter.ino
@@ -1,81 +1,57 @@
-/*----------------------------------------------------------------------*
- * GPS Frequency Counter Example Sketch for Arduino.                    *
- * Uses 1 PPS from a GPS receiver as an accurate time base.             *
- * Developed with Arduino 1.0.3, tested with an Arduino Uno.            *
- * Connect the 1 PPS signal to INT0 (D2 pin).                           *
- * Connect the input signal to be measured to T1 (D5 pin).              *
- * Connect a tactile button switch or similar from the D8 pin to        *
- * ground. This selects the sample period (1, 10, or 100 seconds).      *
- * This example sketch displays the frequency on the Serial monitor     *
- * as well as on a 16x2 LCD. Connecting the LCD is optional.            *
- * Connecting the button is optional if only a 1 second gate period     *
- * is needed.                                                           *
- *                                                                      *
- * Timer 2 can optionally be set to output a 1MHz square wave on the    *
- * MOSI pin (D11). This sketch can then measure its own system clock    *
- * frequency by connecting D11 to D5. This option is on by default;     *
- * to disable, set OUTPUT_1MHZ to zero in the #define statement.        *
- * To measure another MCU's clock, load the t2_1MHz sketch included     *
- * in the examples on the other MCU and connect its D11 pin to D5       *
- * on the MCU running this sketch.                                      *
- *                                                                      *
- * The highest frequency the counter can measure is limited to about    *
- * system clock frequency divided by 2.5, so about 6MHz with a          *
- * 16MHz clock, and about 3MHz with an 8MHz clock.                      *
- *                                                                      *
- * Jack Christensen 24Jan2013                                           *
- *                                                                      *
- * This work is licensed under the Creative Commons Attribution-        *
- * ShareAlike 3.0 Unported License. To view a copy of this license,     *
- * visit http://creativecommons.org/licenses/by-sa/3.0/ or send a       *
- * letter to Creative Commons, 171 Second Street, Suite 300,            *
- * San Francisco, California, 94105, USA.                               *
- *----------------------------------------------------------------------*/ 
+// Arduino gpsFreq Library
+// https://github.com/JChristensen/gpsFreq
+// Copyright (C) 2018 by Jack Christensen and licensed under
+// GNU GPL v3.0, https://www.gnu.org/licenses/gpl.html
+//
+// GPS Frequency Counter Example Sketch.
+// Uses 1 PPS from a GPS receiver as an accurate time base.
+// Tested with Arduino 1.8.19 and an Arduino Uno.
+// Connect the 1 PPS signal to INT0 (D2 pin).
+// Connect the input signal to be measured to T1 (D5 pin).
+// Connect a tactile button switch or similar from the D8 pin to
+// ground. This selects the sample period (1, 10, or 100 seconds).
+// This example sketch displays the frequency on the Serial monitor
+// as well as on a 16x2 LCD. Connecting the LCD is optional.
+// Connecting the button is optional if only a 1 second gate period
+// is needed.
+//
+// The highest frequency the counter can measure is limited to about
+// system clock frequency divided by 2.5, so about 6MHz with a
+// 16MHz clock, and about 3MHz with an 8MHz clock.
+//
+// Jack Christensen 24Jan2013
 
 #include <gpsFreq.h>        // https://github.com/JChristensen/gpsFreq
 #include <JC_Button.h>      // https://github.com/JChristensen/Button
-#include <LiquidCrystal.h>  // https://arduino.cc/en/Reference/LiquidCrystal (included with Arduino IDE)
+#include <LiquidCrystal.h>  // https://arduino.cc/en/Reference/LiquidCrystal
 #include <Streaming.h>      // https://github.com/janelia-arduino/Streaming
 
 //pin assignments
-#define LCD_RS 10                  //16x2 LCD display (your pins may vary)
-#define LCD_EN 11
-#define LCD_D4 A3
-#define LCD_D5 A2
-#define LCD_D6 A1
-#define LCD_D7 A0
-#define GATE_BUTTON 4
-#define GATE_LED LED_BUILTIN
+constexpr uint8_t
+    LCD_RS {10},            // 16x2 LCD display (your pins may vary)
+    LCD_EN {11},
+    LCD_D4 {A3},
+    LCD_D5 {A2},
+    LCD_D6 {A1},
+    LCD_D7 {A0},
+    gateButton {4},
+    gateLED {LED_BUILTIN};
 
-#if F_CPU == 16000000             //calculate OCR2A value for Timer 2
-#define ocr2a 7                   //16MHz, so divide by 8
-#else
-#define ocr2a 3                   //assume 8MHz, so divide by 4
-#endif
-
-unsigned long freq;
-unsigned int nSample;
-boolean gateLedState;
-uint8_t gateTime = 1;
+uint32_t freq;
+uint16_t nSample;
+bool gateLedState;
+uint8_t gateTime {1};
 char strFreq[16];
-Button btnGate(GATE_BUTTON);
+Button btnGate(gateButton);
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 void setup()
 {
-    pinMode(GATE_LED, OUTPUT);
+    pinMode(gateLED, OUTPUT);
     lcd.begin(16, 2);
     btnGate.begin();
     Serial.begin(115200);
     Serial << F( "\n" __FILE__ "\n" __DATE__ " " __TIME__ "\n" );
-
-    #if OUTPUT_1MHZ == 1
-    pinMode(MOSI, OUTPUT);                  //set up Timer 2 for 1MHz
-    TCCR2B = 0;                             //stop timer 2
-    TCCR2A = _BV(WGM21) | _BV(COM2A0);      //CTC mode, toggle OC2A on compare match
-    OCR2A = ocr2a;                    
-    TCCR2B = _BV(CS20);                     //start timer 2, no prescaling
-    #endif
 
     lcd.clear();
     lcd.setCursor(0, 1);
@@ -94,7 +70,7 @@ void loop()
         lcd.setCursor(0, 1);
         lcd << F("Gate: ") << gateTime << F(" sec  ");
     }
-    
+
     if (!gpsFreq.isBusy) {
         if (nSample > 0) {
             gpsFreq.formatFreq(strFreq);
@@ -104,10 +80,9 @@ void loop()
             lcd << strFreq << F(" Hz");
             lcd.setCursor(0, 1);
             lcd << F("Gate: ") << gateTime << F(" sec");
-            digitalWrite(GATE_LED, gateLedState = !gateLedState);
+            digitalWrite(gateLED, gateLedState = !gateLedState);
         }
         ++nSample;
         gpsFreq.start(gateTime);
     }
 }
-
--- examples/t2_1MHz/t2_1MHz.ino
+++ examples/t2_1MHz/t2_1MHz.ino
@@ -1,63 +1,53 @@
-/*----------------------------------------------------------------------*
- * One megahertz frequency reference for Arduino.                       *
- * Outputs a 1MHz square wave on the MOSI (D11) pin.                    *
- * Developed with Arduino 1.0.3, tested with an Arduino Uno.            *
- * Use this sketch in conjunction with the gpsFreqCounter sketch to     *
- * measure the accuracy of the system clock.                            *
- *                                                                      *
- * This sketch will work on MCUs with 16MHz or 8MHz system clock.       *
- *                                                                      *
- * This sketch configures Timer 2 to output a 1MHz square wave on the   *
- * MOSI pin (D11 on Arduino Uno). Since the frequency counter is        *
- * limited to ~6MHz (with 16MHz system clock), we divide the system     *
- * clock down to 1MHz, which makes it easy to estimate PPM accuracy     *
- * directly from the frequency measurment.                              *
- *                                                                      *
- * Jack Christensen 24Jan2013                                           *
- *                                                                      *
- * This work is licensed under the Creative Commons Attribution-        *
- * ShareAlike 3.0 Unported License. To view a copy of this license,     *
- * visit http://creativecommons.org/licenses/by-sa/3.0/ or send a       *
- * letter to Creative Commons, 171 Second Street, Suite 300,            *
- * San Francisco, California, 94105, USA.                               *
- *----------------------------------------------------------------------*/ 
+// Arduino gpsFreq Library
+// https://github.com/JChristensen/gpsFreq
+// Copyright (C) 2018 by Jack Christensen and licensed under
+// GNU GPL v3.0, https://www.gnu.org/licenses/gpl.html
+//
+// Example sketch: One megahertz frequency reference.
+// Outputs a 1MHz square wave on the MOSI (D11) pin.
+// Tested with Arduino 1.8.19 and an Arduino Uno.
+// Use this sketch in conjunction with the gpsFreqCounter
+// sketch to measure the accuracy of the system clock.
+//
+// This sketch will work on MCUs with 16MHz or 8MHz system clock.
+//
+// Timer 2 is configured to output a 1MHz square wave on the
+// MOSI pin (D11 on Arduino Uno). Since the frequency counter is
+// limited to ~6MHz (with 16MHz system clock), we divide the system
+// clock down to 1MHz, which makes it easy to estimate PPM accuracy
+// directly from the frequency measurement.
+//
+// Jack Christensen 24Jan2013
 
-#define BLINK_THAT_LED 0          //optionally set to 1 to blink the built-in LED
+constexpr bool blink {false};               // set to true to blink an LED
+constexpr uint8_t blinkLED {LED_BUILTIN};   // the pin number of the LED to blink
+constexpr uint32_t blinkInterval {1000};    // blink interval, milliseconds
 
-#define LED LED_BUILTIN           //LED pin number
-#define LED_INTERVAL 1000
-
-#if F_CPU == 16000000             //calculate OCR2A value for Timer 2
-#define ocr2a 7                   //16MHz, so divide by 8
-#else
-#define ocr2a 3                   //assume 8MHz, so divide by 4
-#endif
-
-void setup(void)
+void setup()
 {
-    #if BLINK_THAT_LED == 1
-    pinMode(LED, OUTPUT);
-    #endif
-    pinMode(11, OUTPUT);                    //D11, MOSI, PB3, DIP 17
-    //Set timer 2 to output a 1MHz square wave on MOSI (D11 on Arduino Uno)
-    TCCR2B = 0;                             //stop timer 2
-    TCCR2A = _BV(WGM21) | _BV(COM2A0);      //CTC mode, toggle OC2A on compare match
-    OCR2A = ocr2a;                    
-    TCCR2B = _BV(CS20);                     //start timer 2, no prescaling    
+    if (blink) pinMode(blinkLED, OUTPUT);
+    pinMode(MOSI, OUTPUT);                  // D11, MOSI, PB3, DIP 17
+
+    // Set timer 2 to output a 1MHz square wave on MOSI (D11 on Arduino Uno)
+    TCCR2B = 0;                             // stop timer 2
+    TCCR2A = _BV(WGM21) | _BV(COM2A0);      // CTC mode, toggle OC2A on compare match
+    // calculate OCR2A value for Timer 2. For 16MHz, divide by 8,
+    // else assume 8MHz and divide by 4.
+    constexpr uint32_t cpuFreq {F_CPU};
+    OCR2A = (cpuFreq == 16000000) ? 7 : 3;
+    TCCR2B = _BV(CS20);                     // start timer 2, no prescaling
 }
 
-void loop(void)
+void loop()
 {
-    #if BLINK_THAT_LED == 1
-    //nothing else to do here, so we might as well blink an LED ;-)
-    static unsigned long ms, msLast;
-    static boolean ledState;
+    if (blink) {
+        static uint32_t ms, msLast;
+        static bool ledState;
 
-    ms = millis();
-    if (ms - msLast >= LED_INTERVAL) {
-        msLast = ms;
-        digitalWrite(LED, ledState = !ledState);
+        ms = millis();
+        if (ms - msLast >= blinkInterval) {
+            msLast = ms;
+            digitalWrite(blinkLED, ledState = !ledState);
+        }
     }
-    #endif
 }
-
